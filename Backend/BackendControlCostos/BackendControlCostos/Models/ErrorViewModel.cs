862b399bdeaca74255ba36491c014875ef711e6f
MariaVegaAlcantara <maguichis.mary@gmail.com>
2020-07-21 21:04:19 -0500

    Firts

---
+++ Backend/BackendControlCostos/BackendControlCostos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BackendControlCostos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
